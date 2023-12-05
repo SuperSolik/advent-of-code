051a0f214772f48af04e4de55b9e4042a573e965
SuperSolik <serpetr99@gmail.com>
2023-12-05 16:01:09 +0100

    added solution template

---
+++ solution_template.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import fileinput
+
+
+def solve(lines: list[str]) -> None:
+    print(lines)
+    pass
+    
+
+if __name__ == '__main__':
+    print(solve([line for line in fileinput.input()]))

871e9800f64e47439224dc07b98a6348f380a1b4
SuperSolik <serpetr99@gmail.com>
2023-12-09 14:06:37 +0100

    upd solution template

--- solution_template.py
+++ solution_template.py
@@ -3,8 +3,7 @@ import fileinput
 
 def solve(lines: list[str]) -> None:
     print(lines)
-    pass
     
 
 if __name__ == '__main__':
-    print(solve([line for line in fileinput.input()]))
+    solve([line for line in fileinput.input()])

e764f7fbb4bced5e1127df6aff4963e5dcfb90a4
SuperSolik <serpetr99@gmail.com>
2024-12-04 18:23:09 +0100

    2024 day4 in go

---
+++ 2024/day4/p1/run.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func parseInput() []string {
+	args := os.Args[1:]
+
+	if len(args) <= 0 {
+		log.Fatal("Input file is not provided")
+	}
+	inputFilePath, err := filepath.Abs(args[0])
+	if err != nil {
+		log.Fatalf("Unable to create abs path from: %s\n", args[0])
+	}
+
+	log.Printf("Input file: %s", inputFilePath)
+
+	bytes, err := os.ReadFile(inputFilePath)
+
+	if err != nil {
+		log.Fatalf("Unable to read file: %s\n", inputFilePath)
+	}
+
+	return strings.Split(string(bytes), "\n")
+}
+
+// func printSlice[T any](arr []T, newline bool) {
+// 	for _, v := range arr {
+// 		fmt.Printf("%v ", v)
+// 	}
+// 	if newline {
+// 		fmt.Println()
+// 	}
+// }
+
+type Solution struct {
+	lines    []string
+	lineSize int
+	size     int
+}
+
+func (s *Solution) LookAt(x, y int) (byte, error) {
+	if x >= 0 && x < s.lineSize && y >= 0 && y < s.size {
+		return s.lines[y][x], nil
+	}
+
+	return byte(0), fmt.Errorf("%d:%d out of bounds", x, y)
+}
+
+const xmas = "XMAS"
+
+func (s *Solution) CheckXmas(x, y, dx, dy int) (bool, error) {
+	bytes := make([]byte, 4, 4)
+	bytes[0] = 'X'
+
+	i := 1
+
+	for i < len(xmas) {
+		x += dx
+		y += dy
+		c, err := s.LookAt(x, y)
+		log.Printf("%c at %d:%d\n", c, x, y)
+
+		if err != nil {
+			log.Println(err)
+			return false, err
+		}
+		bytes[i] = c
+		i += 1
+	}
+
+	res := string(bytes) == xmas
+
+	return res, nil
+}
+
+func (s *Solution) CountXmas(x, y int) int {
+	res := 0
+	for dy := -1; dy <= 1; dy++ {
+		for dx := -1; dx <= 1; dx++ {
+			if dx == 0 && dy == 0 {
+				continue
+			}
+
+			yes, _ := s.CheckXmas(x, y, dx, dy)
+
+			if yes == true {
+				fmt.Printf("Found %s at (%d, %d) with dir = (%d, %d)\n", xmas, x, y, dx, dy)
+				res += 1
+			}
+		}
+	}
+	return res
+}
+
+func (s *Solution) Solve() int {
+	res := 0
+	for y := 0; y < s.size; y++ {
+		for x := 0; x < s.lineSize; x++ {
+			if s.lines[y][x] == 'X' {
+				res += s.CountXmas(x, y)
+			}
+		}
+	}
+	return res
+}
+
+func main() {
+	content := parseInput()
+
+	solution := Solution{lines: content, lineSize: len(content[0]), size: len(content)}
+
+	fmt.Printf("lineSize = %d, size = %d\n", solution.lineSize, solution.size)
+
+	fmt.Printf("%d\n", solution.Solve())
+}
---
+++ 2024/day4/p2/run.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func parseInput() []string {
+	args := os.Args[1:]
+
+	if len(args) <= 0 {
+		log.Fatal("Input file is not provided")
+	}
+	inputFilePath, err := filepath.Abs(args[0])
+	if err != nil {
+		log.Fatalf("Unable to create abs path from: %s\n", args[0])
+	}
+
+	log.Printf("Input file: %s", inputFilePath)
+
+	bytes, err := os.ReadFile(inputFilePath)
+
+	if err != nil {
+		log.Fatalf("Unable to read file: %s\n", inputFilePath)
+	}
+
+	return strings.Split(string(bytes), "\n")
+}
+
+// func printSlice[T any](arr []T, newline bool) {
+// 	for _, v := range arr {
+// 		fmt.Printf("%v ", v)
+// 	}
+// 	if newline {
+// 		fmt.Println()
+// 	}
+// }
+
+type Solution struct {
+	lines    []string
+	lineSize int
+	size     int
+}
+
+func (s *Solution) LookAt(x, y int) (byte, error) {
+	if x >= 0 && x < s.lineSize && y >= 0 && y < s.size {
+		return s.lines[y][x], nil
+	}
+
+	return byte(0), fmt.Errorf("%d:%d out of bounds", x, y)
+}
+
+func (s *Solution) CheckTarget(x, y, dx, dy int, target string) (bool, error) {
+	l := len(target)
+	bytes := make([]byte, l, l)
+
+	i := 0
+
+	for i < l {
+		c, err := s.LookAt(x, y)
+
+		if err != nil {
+			log.Println(err)
+			return false, err
+		}
+		log.Printf("%c at %d:%d\n", c, x, y)
+		bytes[i] = c
+
+		x += dx
+		y += dy
+		i += 1
+	}
+
+	res := string(bytes) == target
+
+	return res, nil
+}
+
+func (s *Solution) CheckXmas(x, y int) bool {
+
+	mas1, _ := s.CheckTarget(x-1, y-1, 1, 1, "MAS")
+	sam1, _ := s.CheckTarget(x-1, y-1, 1, 1, "SAM")
+	mas2, _ := s.CheckTarget(x+1, y-1, -1, 1, "MAS")
+	sam2, _ := s.CheckTarget(x+1, y-1, -1, 1, "SAM")
+
+	if (mas1 || sam1) && (mas2 || sam2) {
+		return true
+	}
+
+	return false
+}
+
+func (s *Solution) Solve() int {
+	res := 0
+	for y := 1; y < s.size-1; y++ {
+		for x := 1; x < s.lineSize-1; x++ {
+			if s.lines[y][x] == 'A' && s.CheckXmas(x, y) {
+				res += 1
+			}
+		}
+	}
+	return res
+}
+
+func main() {
+	content := parseInput()
+
+	solution := Solution{lines: content, lineSize: len(content[0]), size: len(content)}
+
+	fmt.Printf("lineSize = %d, size = %d\n", solution.lineSize, solution.size)
+
+	fmt.Printf("%d\n", solution.Solve())
+}

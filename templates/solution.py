0c75603ba090b4e3f69e15094a49845773e72352
SuperSolik <serpetr99@gmail.com>
2025-12-01 22:23:55 +0100

    add python solution template

---
+++ templates/solution.py
@@ -0,0 +1,16 @@
+import fileinput
+from typing import Iterable
+
+
+def solve1(lines: Iterable[str]) -> None:
+    pass
+
+
+def solve2(lines: Iterable[str]) -> None:
+    pass
+
+
+if __name__ == "__main__":
+    data = [line.strip() for line in fileinput.input()]
+    print("Part1: ", solve1(data))
+    print("Part2: ", solve2(data))
